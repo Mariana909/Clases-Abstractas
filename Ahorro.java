64b9c730388517a97309f0ce3ed9c9308cbb127c
Mariana909 <karla.duque01@usa.edu.co>
2024-10-17 12:07:07 -0500

    Update Ahorro.java

--- Ahorro.java
+++ Ahorro.java
@@ -3,16 +3,20 @@ package ejercicio2;
 public class Ahorro extends Cuenta{
 	private long interes;
 
-	public Ahorro(int numeroCuenta, String titular, long interes) {
+	public Ahorro(int numeroCuenta, String titular) {
 		super(numeroCuenta, titular);
-		this.interes = interes;
 	}
 
 	@Override
 	public void deposito(long valor) {
 		// TODO Auto-generated method stub
-		this.saldo+=valor;
-		this.interes=(long) (saldo*0.03);
+		if(valor>0) {
+			this.saldo+=valor;
+			this.interes+=(long) (saldo*0.03);
+			System.out.println("Interes: "+interes+" Saldo: "+saldo);
+		}else {
+			System.out.println("No es posible depositar valores negativos");
+		}
 	}
 
 	@Override
@@ -21,6 +25,7 @@ public class Ahorro extends Cuenta{
 		if(valor<=saldo)
 		{
 			this.saldo=saldo-valor;
+			System.out.println("Interes: "+interes+" Saldo: "+saldo);
 		}else {
 			System.out.println("Saldo insuficiente");
 		}
